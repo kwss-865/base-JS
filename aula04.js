f10f530b9de317a4e606942c437ee5a2638a0a52
kwss-865 <134401209+kwss-865@users.noreply.github.com>
2024-08-22 12:07:35 -0300

    finalizando base.js

--- aula04.js
+++ aula04.js
@@ -46,3 +46,32 @@ function multiplicao(numero4 = 2, numero5 = 5){
     return numero4 * numero5
 }
 imprimeTexto(multiplicao(somaDeNumeros(3,4),somaDeNumeros(5,10)));
+
+// expressão de função 
+
+const constanteSoma = function(nume1, nume2){return nume1 + nume2} 
+
+console.log(constanteSoma(3,6))
+
+// Funções e var são "listadas" no topo do arquivo
+
+// Arrow funtion 
+
+const appresentaArrow = nome => `meu nome é ${nome}`;
+const somaEmFlecha = (nu1, nu2) => nu1 + nu2;
+
+console.log(appresentaArrow("Tuf"))
+console.log(somaEmFlecha(2,4)) 
+
+const somaNumerosPequenos = (num1,num2) => {
+    if(num1 >10 || num2 > 10){
+        return "Essa função é apenas para números pequenos"
+    }
+    else {
+        return num1 + num2;
+    }
+}
+
+console.log(somaNumerosPequenos(2,3))
+
+
